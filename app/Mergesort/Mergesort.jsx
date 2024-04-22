2ada75715b6af373282d5392af28e52eadcbd8cf
haileypham <haileympham@gmail.com>
2024-04-22 18:10:53 -0400

    Update Mergesort.jsx

--- app/Mergesort/Mergesort.jsx
+++ app/Mergesort/Mergesort.jsx
@@ -5,21 +5,20 @@ import styles from './Mergesort.module.css';
 // sorting them with mergesort
 export default function Mergesort(resultList) {
     if (resultList.length == 0) {
-        // console.log("result list is invalid");
         return;
     }
 
     let mergeSortedArr = resultList.map(arr => [...arr]);
     console.log("Merge Sorted Array: ", mergeSortedArr);
     
+    // merge two arrays
     function merge(arr, l, m, r) {
         var n1 = parseInt(m) - l +1;
         var n2 = r-m;
-        // var n1 = m - l + 1;
-        // var n2 = r - m;
 
         var L = new Array(n1);
         var R = new Array(n2);
+
 
         for (i = 0; i < n1; i++) {
             L[i] = arr[l+i];
@@ -32,8 +31,9 @@ export default function Mergesort(resultList) {
         var j = 0;
         var k = l;
 
+        // while lists not empty, add next highest element
         while(i < n1 && j < n2) {
-            if(L[i][1] <= R[j][1]) {
+            if(L[i][1] >= R[j][1]) {
                 arr[k] = L[i];
                 i++;
             }
@@ -44,12 +44,12 @@ export default function Mergesort(resultList) {
             k++;
         }
 
+        // add the rest of the elements
         while(i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
-
         while(j < n2) {
             arr[k] = R[j];
             j++;
@@ -58,10 +58,13 @@ export default function Mergesort(resultList) {
     }
 
     function mergeSort(arr, l, r) {
+        // base case, if left is greater than right
         if (l >= r) {
             return;
         }
+
         var m = l + parseInt((r-l)/2);
+        
         mergeSort(arr,l,m);
         mergeSort(arr,m+1,r);
         merge(arr,l,m,r);
@@ -69,7 +72,7 @@ export default function Mergesort(resultList) {
 
     // return the results displayed in the sorted order
     mergeSort(mergeSortedArr, 0, mergeSortedArr.length-1);
+    console.log("MERGE SORT");
     console.log("Merge Sorted Array: ", mergeSortedArr);
     return mergeSortedArr;    // merges two arrays
-    // return mergeSort(resultList, 0, resultList.length-1);
 }
